e111ba594f7d9e929425b7962b320c4333ab7575
ShotAlex <shot.aleks@gmail.com>
2020-09-14 22:01:09 +0300

    feat: added: solution of the brackets task

--- src/index.js
+++ src/index.js
@@ -1,41 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-    //  I have no idea how I can use the second parameter in this example. 
-    //  Because my solution is this
-    if (str.length % 2 !== 0 || !str.length) return false;
-    let c = [];
+    str = str.split('')
+    let arr = []
 
-    for (let i = 0; i < str.length; i++) {
-        if ( str[i] == '(' || str[i] == '{' || str[i] == '[') {
-            c.push(str[i]);
-            
-        } else if (str[i] == ')' || str[i] == '}' || str[i] == ']') {
-            if (c.length == 0) {
-                return false;
-            } else if (c[c.length-1] == '(' && str[i] == ')' || c[c.length-1] == '{' && str[i] == '}' || c[c.length-1] == '[' && str[i] == ']') {
-                c.pop();
-            } else {
-                return false;
-            }
-        }
+    if (str.length % 2 !== 0) return false
 
-        if (str[i] == '|' ||
-        str[i] == '1' ||
-        str[i] == '2' ||
-        str[i] == '3' ||
-        str[i] == '4' ||
-        str[i] == '5' ||
-        str[i] == '6' ||
-        str[i] == '7' ||
-        str[i] == '8' ||
-        str[i] == '9') {
-            if (str[i] != c[c.length-1]) {
-                c.push(str[i]);
-            } else {
-                c.pop();
-            }
+    for(let i = 0; i < str.length; i++){
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if(str[i] === bracketsConfig[j][1] && arr[arr.length - 1] === bracketsConfig[j][0]) arr.pop()
+            else if(str[i] === bracketsConfig[j][0]) arr.push(str[i])
         }
     }
-
-    if (c.length > 0) return false;
-    return true;
+    
+    return arr.length === 0
 }
